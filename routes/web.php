03cb070b410d4df125f76328570bf7ed60817c8f
Cesar <cesar_silk321@hotmail.com>
2021-02-04 10:51:54 -0300

    documentação

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('docs', function () {
     return view('docs');
+});
+
+Route::get('/', function () {
+    return view('welcome');
 });
