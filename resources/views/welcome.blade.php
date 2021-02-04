f5d2d0016ec56a4db9a73c6218d21dca595cee36
Cesar <cesar_silk321@hotmail.com>
2021-02-04 10:47:14 -0300

    documentação

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,29 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf8">
-    <meta name="generator" content="Insomnia Documenter v0.4.1">
-    <meta name="viewport" content="width=device-width, user-scalable=no">
-    <link rel="shortcut icon" href="favicon.ico">
-    <link rel="stylesheet" href="bundle.css">
-    <script defer src="bundle.js"></script>
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
-          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    </head>
     <style>
-        header {
-            border-top: 6px solid rgb(233, 131, 0)!important;
+        body {
+            background: url("brasal.jpeg") no-repeat;
+            background-size: cover;
+            position: relative;
+            padding: 0;
+            margin: 0;
+        }
+        body::after {
+            content: '';
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100vw;
+            height: 100vh;
+            background: rgba(0,0,0,.7);
         }
     </style>
-</head>
-<body>
-<noscript>In order to view this documentation page, you have to enable JavaScript in your web browser.</noscript>
-<div id="app"></div>
-</body>
+    <body>
+    </body>
 </html>
